fbfa0fc583553602d33a2c84d1f6736799c8b738
Jeremias Diaz <monitz87@gmail.com>
2019-11-09 11:00:11 -0500

    feat: create consumption endpoint

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const needle = require('needle');
+const cheerio = require('cheerio');
+
+const app = express();
+app.get('/consumption', async (_req, res) => {
+  needle.get('https://digiconomist.net/ethereum-energy-consumption', (_err, resp) => {
+    const $ = cheerio.load(resp.body);
+    const node = $('#table_31_row_8 td:last-child');    
+    
+    res.json({ result: parseInt(node.text(), 10) });
+  });
+});
+
+app.listen(3000, () => console.log('listening'));
